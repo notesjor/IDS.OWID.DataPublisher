972c400d6cc109d590ea04db02622a40cc719644
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2023-08-11 18:43:16 +0200

    // commit

--- Tools/IDS.OWID.DataPublisher.Tool.Prepper/RadForm1.cs
+++ Tools/IDS.OWID.DataPublisher.Tool.Prepper/RadForm1.cs
@@ -52,20 +52,58 @@ namespace IDS.OWID.DataPublisher.Tool.Prepper
       var path = Path.GetDirectoryName(_path);
       var name = Path.GetFileNameWithoutExtension(_path);
 
-      File.Copy(_path, Path.Combine(path, name + ".sec.json"), true);
+      var columns = GetColumns();
+      var copy = _data.Select(x => x.ToDictionary(y => columns[y.Key], y => y.Value)).ToArray();
+
+      File.WriteAllText(Path.Combine(path, name + ".sec.json"), JsonConvert.SerializeObject(copy));
+      File.WriteAllText(Path.Combine(path, name + ".sec.schema.json"), JsonConvert.SerializeObject(GetSchema(columns, copy.First())));
 
       var hideKeys = new HashSet<string>(list.Items.Where(x => x.CheckState == Telerik.WinControls.Enumerations.ToggleState.On).Select(x => x.Text));
 
-      foreach (var item in _data)
+      copy = _data.Select(x => x.Where(y => !hideKeys.Contains(y.Key)).ToDictionary(y => columns[y.Key], y => y.Value)).ToArray();
+      File.WriteAllText(Path.Combine(path, name + ".pub.json"), JsonConvert.SerializeObject(copy));
+      File.WriteAllText(Path.Combine(path, name + ".pub.schema.json"), JsonConvert.SerializeObject(GetSchema(columns, copy.First())));
+      Close();
+    }
+
+    private Dictionary<string, string>[] GetSchema(Dictionary<string, string> columns, Dictionary<string, object> sample)
+    {
+      var res = new List<Dictionary<string, string>>();
+      foreach (var item in columns)
       {
-        foreach (var k in hideKeys)
-          if (item.ContainsKey(k))
-            item.Remove(k);
+        var name = item.Value;
+        if(!sample.ContainsKey(name))
+          continue;
+        var type = sample[name]?.GetType();
+
+        var obj = new Dictionary<string, string> {
+          { "name", name },
+          { "caption", item.Key },
+          { "headerText", item.Key },
+          { "width", "140" }
+        };
+
+        if (type == typeof(int))
+          obj.Add("type", "number");
+        else if (type == typeof(double))
+          obj.Add("type", "number");
+        else if (type == typeof(DateTime))
+          obj.Add("type", "date");
+        else
+          obj.Add("type", "string");
+
+        res.Add(obj);
       }
 
-      File.WriteAllText(Path.Combine(path, name + ".pub" +
-        ".json"), JsonConvert.SerializeObject(_data));
-      Close();
+      return res.ToArray();
+    }
+
+    private Dictionary<string, string> GetColumns()
+    {
+      var res = new Dictionary<string, string>();
+      foreach (var key in _keys)
+        res.Add(key, $"c{res.Count:D2}");
+      return res;
     }
   }
 }
--- WebApp/pages/table.vue
+++ WebApp/pages/table.vue
@@ -1,15 +1,54 @@
 <template>
     <div>
-
+      <ejs-grid ref="grid" :columns="schema" :dataSource="data" :allowSorting="true" :allowGrouping="true" :allowFiltering="true" :allowPaging="true" :pageSettings="pageSettings">
+      </ejs-grid>
     </div>
-</template>
+  </template>
+    
+  <script>
+  import { GridComponent, ColumnsDirective, ColumnDirective, Sort, Group, Filter, Page } from '@syncfusion/ej2-vue-grids';
 
-<script>
-    export default {
-        
-    }
-</script>
+  export default {
+    name: "ViewGrid",
+    components: {
+      'ejs-grid': GridComponent,
+      'e-columns': ColumnsDirective,
+      'e-column': ColumnDirective
+    },
+    data() {
+      return {
+        schema: [],
+        data: [],
+        pageSettings: { pageSize: 15, pageSizes: ['10', '15', '20', '25', '50', '100', '250', '500', '1000', 'All'] }
+      };      
+    },
+    mounted(){
+      fetch('/data.pub.schema.json')
+      .then(response => response.json())
+      .then(data => {
+        this.schema = data;
+      });
 
-<style lang="scss" scoped>
-
-</style>
+      fetch('/data.pub.json')
+      .then(response => response.json())
+      .then(data => {
+        this.data = data;
+      });
+    },
+    provide: {
+      grid: [Sort, Group, Filter, Page],
+    },
+  };
+  </script>
+    
+  <style>
+  @import "@syncfusion/ej2-base/styles/material.css";
+  @import "@syncfusion/ej2-inputs/styles/material.css";
+  @import "@syncfusion/ej2-buttons/styles/material.css";
+  @import "@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "@syncfusion/ej2-lists/styles/material.css";
+  @import "@syncfusion/ej2-popups/styles/material.css";
+  @import "@syncfusion/ej2-popups/styles/material.css";
+  @import "@syncfusion/ej2-navigations/styles/material.css";
+  @import "@syncfusion/ej2-grids/styles/material.css";
+  </style>
